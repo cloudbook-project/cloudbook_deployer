f53a23302d0c2c1dd5084c87195438b5beb1428e
jjaranda13 <jjaranda13@alcatel-lucent.com>
2018-09-11 17:02:21 +0200

    deployer_proto

--- cloudbook_deployer.py
+++ cloudbook_deployer.py
@@ -1,22 +1,22 @@
 import json
 
-cloudbook={}
 
-cloudbook={
+
+cloudbook_agents={
 	"agent_0" : {"127.0.0.1":["du_0"]},
 	"agent_1" : {"127.0.0.1":["du_1"]}
 }
-json_str = json.dumps(cloudbook)
-fo = open("../cloudbook_agent/du_files/cloudbook.json", 'w')
+json_str = json.dumps(cloudbook_agents)
+fo = open("../cloudbook_agent/du_files/cloudbook_agents.json", 'w')
 fo.write(json_str)
 fo.close()
 
-cloudbook_du={
+cloudbook_dus={
 	"du_0" : ["agent_0"],
 	"du_1" : ["agent_1"]
 }
-json_str = json.dumps(cloudbook_du)
-fo = open("../cloudbook_agent/du_files/cloudbook_du.json", 'w')
+json_str = json.dumps(cloudbook_dus)
+fo = open("../cloudbook_agent/du_files/cloudbook_dus.json", 'w')
 fo.write(json_str)
 fo.close()
 
