9c25dfa7949f83ce15e7f0c0ed79a4ab538be0f3
jjaranda13 <jjaranda13@alcatel-lucent.com>
2018-09-14 15:54:05 +0200

    proto

--- cloudbook_deployer.py
+++ cloudbook_deployer.py
@@ -6,12 +6,12 @@ cloudbook_agents={
 	"agent_0" : {"127.0.0.1":["du_0"]},
 	"agent_1" : {"127.0.0.1":["du_1"]}
 }
-
+"""
 cloudbook_dus={
 	"du_0" : ["agent_0"],
 	"du_1" : ["agent_1"]
 }
-
+"""
 def assign_dus_to_machines(machines, dus):
 	
 	# example of circle.json
@@ -59,8 +59,10 @@ fo = open("../cloudbook_agent/du_files/cloudbook_agents.json", 'w')
 fo.write(json_str)
 fo.close()
 
+"""
 json_str = json.dumps(cloudbook_dus)
 fo = open("../cloudbook_agent/du_files/cloudbook_dus.json", 'w')
 fo.write(json_str)
 fo.close()
+"""
 
