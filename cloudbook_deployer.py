d46b0234983da7f8059f84febdc7ccd22a961373
Garcia Aranda <jose_javier.garcia_aranda@nokia.com>
2019-05-27 14:02:34 +0200

    du0 well assigned

--- cloudbook_deployer.py
+++ cloudbook_deployer.py
@@ -1,6 +1,9 @@
 import json
 import loader
 import operator
+import platform
+import os
+import sys
 
 """
 	# example of what circle manager must return
@@ -136,7 +139,7 @@ def assign_dus_to_machines_old(circle_agents, agents_with_grant, dus, configurat
 
 
 def assign_dus_to_machines(circle_agents, agents_with_grant, dus, configuration = None):
-
+	print ("ENTER in assign_dus_to_machines()...")	
 	#dus_sorted=sort_dus(dus) # this is a list of tuples
 	#agents_sorted_by_grant=sort_agents(agents_with_grant) # this is a list of tuples
 
@@ -150,12 +153,12 @@ def assign_dus_to_machines(circle_agents, agents_with_grant, dus, configuration 
 	#Each DU has a number of performance points assigned. When we distribute DUs amongs agents, we substract the performance of 
 	# every agent to the performance of every DU, assigning it when the value is closest to zero. We do this in every case.
 	#With this method we are always assigning every DU to an agent, no matter if there are more DUs that agents.
-		
+	
 	result = {}
 	total_dus=len(dus)
-	print "there are ", total_dus," DUs"
-	print "dus", dus
-	print "agents",agents_with_grant
+	print "  there are ", total_dus," DUs"
+	print "  dus", dus
+	print "  agents",agents_with_grant
 
 
 	for a in agents_with_grant:
@@ -166,15 +169,44 @@ def assign_dus_to_machines(circle_agents, agents_with_grant, dus, configuration 
 		else:
 			agents_with_grant[a]=100	
 
+	# assign the DU0 to the agent0 
+	#------------------------------
+	print (" always DU0 is assigned to agent0 ")
+	for du in dus:
+		if du=="du_0":
+			du0=du
+			#print "du_0 found"
+			break
+
+	for a in agents_with_grant:
+		if a=="AGENT0":
+			#print "agent0 found"
+			try:
+				result[du0].append(a)
+			except:
+				result[du0]=[]	
+				result[du0].append(a)
+			agents_with_grant[a]=int(agents_with_grant[a])-int(dus[du0]["cost"]+dus[du0]["size"])
+			dus[du0]["cost"]=0
+			break
+			#print "choosen DU:",dus[du0]
+			#print "choosen agent:",agents_with_grant[a]
 
 	for i in range(0,total_dus):
-		#search most costly DU
+		#----------------------------------------------
+		# search most costly DU, then search the most powerfull agent
+		# assign the DU to the agent and reduce the power of the agent
+		# delete the DU from du list
+		# repeat the process till none DU is remaining 
+		#-----------------------------------------------
 		maxcost=0
 		for du in dus:
-			if (dus[du]["cost"]+dus[du]["size"]>maxcost):
+			if (dus[du]["cost"]+dus[du]["size"]>=maxcost):
 				max_du=du
 				maxcost=dus[du]["cost"]+dus[du]["size"]
 
+		if maxcost==0:
+			break
 		#print "the max costly DU is ", max_du
 
 		#search the most powerfull agent
@@ -190,19 +222,23 @@ def assign_dus_to_machines(circle_agents, agents_with_grant, dus, configuration 
 		
 
 		if (max_agent==""):
+			print ("  failure searching the most powefull agent")
 			return False
 
 		
 		#if (int(agents_with_grant[max_agent])<int (dus[max_du]["cost"]+dus[max_du]["size"])):
 		#	return False
 		
+
+		# we assign the most costly DU to the most powerfull agent
+		# ----------------------------------------------------------
 		print "choosen DU:",max_du,dus[max_du]
 		print "choosen agent:",max_agent,agents_with_grant[max_agent]
 		#print "power es ", agents_with_grant[max_agent]
 		#print "le restamos ",dus[max_du]
 		agents_with_grant[max_agent]=int(agents_with_grant[max_agent])-int(dus[max_du]["cost"]+dus[max_du]["size"])
 		dus[max_du]["cost"]=0
-		dus[max_du]["size"]=0
+		
 
 		try:
 			result[max_du].append(max_agent)
@@ -210,7 +246,12 @@ def assign_dus_to_machines(circle_agents, agents_with_grant, dus, configuration 
 			result[max_du]=[]	
 			result[max_du].append(max_agent)
 		#print(chosen_agent, agents_sorted_by_grant[chosen_agent])
+
+
+
+
 	#TODO: Make this properly, temporal assignation of du_0
+	"""
 	aux_value = ""
 	print "===Correct du assignation==="
 	for key, value in result.iteritems():
@@ -220,7 +261,7 @@ def assign_dus_to_machines(circle_agents, agents_with_grant, dus, configuration 
 			
 		if (value == ["agent0"]) and (key != "du_0"):
 			result[key] = aux_value
-
+	"""
 	print "result is", result
 
 	return result
@@ -269,14 +310,21 @@ def deploy(circle_id, configuration = None):
 #Performs full local deployment and assignment of DUs to agents
 def deploy_local(agents_in_local_circle, path, configuration = None):
 
-	#transform str in json format into dictionary
+	#transform str (in json format) into dictionary with pairs agent, grant
 	data = json.loads(agents_in_local_circle)
+
+	#create a list of agents without grant
 	agents_list = list(data.keys())
 	print ("list of agents:")
 	print (agents_list)
 
+	#assign DUs to machines
+	#-----------------------
+	# NOTE: both variables "agents_with_grant and dus are global, not need to pass"
 	res = assign_dus_to_machines(agents_list, agents_with_grant, dus)
-
+	
+	#write output file in json format
+	#---------------------------------
 	json_str = json.dumps(res)
 	fo = open(output_dir+"/cloudbook.json", 'w')
 	fo.write(json_str)
@@ -290,8 +338,32 @@ def load_dictionary(filename):
 		aux = json.load(file)
 	return aux
 
-input_dict = load_dictionary("./config_deployer.json")
 
+#main program to execute by command line
+#=======================================
+print ("Welcome to cloudbook deployer  (V1.0)")
+print ("=====================================")
+print (" usage :")
+print ("   python deploy.py [-mode local|service]")
+print ("  ")	
+print (" Prerequisites:")
+print (" 1. default folder must exist:")
+print ("    - windows: $HOMEDRIVE/$HOMEPATH/cloudbook/")
+print ("    - linux: /etc/cloudbook/")
+print (" 2. input files must exist at default folder:")
+print ("    - config.json : common for all cloudbook components")
+print ("    - du_list.json : created by cloudbook maker component")
+print ("    - agents_grant.json : provided by agents")
+print (" ")
+print (" Output files:")
+print ("  - cloudbook.json: this file contains the assignment of DUs to agents")
+print ("=======================================")
+print ( "")
+
+
+# access to default directory or create it, if it does not exist
+#------------------------------------------------------------
+input_dict = load_dictionary("./config.json")
 if input_dict["circle_info"]["DISTRIBUTED_FS"] == "":
 	if(platform.system()=="Windows"):
 	    path= os.environ['HOMEDRIVE'] + os.environ['HOMEPATH']+"/cloudbook/"
@@ -304,29 +376,53 @@ if input_dict["circle_info"]["DISTRIBUTED_FS"] == "":
 else:
 	path = input_dict["circle_info"]["DISTRIBUTED_FS"] 
 
-input_dir = path#input_dict["circle_info"]["DISTRIBUTED_FS"]#["input_folder"]
-output_dir = path#input_dict["circle_info"]["DISTRIBUTED_FS"]#["output_folder"]
+#the input and output path is the same
+#-------------------------------------
+input_dir = path
+output_dir = path
 
 #This file must exist in the cloudbook folder, created by the Maker
+#-------------------------------------------------------------------
 dus=loader.load_dictionary(input_dir+"/du_list.json")
 
-#This file must exit in the cloudbook folder, created by the agents.
-agents_with_grant = loader.load_dictionary(input_dir+"/agent_grant.json")
+#This file must exit in the cloudbook folder, created by the agents
+#-------------------------------------------------------------------
+agents_with_grant = loader.load_dictionary(input_dir+"/agents_grant.json")
 
-#Get agents from Circle, for now, we're using a fake:
-#Just an example of what circle manager must return
-"""
-cloudbook_agents={
-	"agent_id_0",
-	"agent_id_1"
-	}
-"""
 
-agents_in_local_circle=json.dumps(agents_with_grant)
+# in "service" working mode, the agents must be requested to the circle manager service
+# in "local" mode, we use angents_grant.json
+#------------------------------------------------------------------------------------
+# example of agents_grant.json
+#  {
+#  "AGENT0":"MEDIUM",
+#  "AGENT1":"LOW",
+#  "AGENT2":"LOW"
+#  }
+
+mode="local"
+num_param=len(sys.argv)
+if num_param==3 :
+	if sys.argv[1]=="-mode":
+		mode=sys.argv[2]
+		
+
+print ("working mode:"), mode
+
+if mode=="local":
+	#all content is loaded as single string into a variable.
+	agents_in_local_circle=json.dumps(agents_with_grant)
+else:
+	print ("Service mode is not supported in this version")
+	sys.exit()
+
+
 
 print ("Agents in local circle:")
 print agents_in_local_circle
 
+#this is the most important function of deployer
+#-----------------------------------------------
 deploy_local(agents_in_local_circle, ".", configuration = None)
 
 
