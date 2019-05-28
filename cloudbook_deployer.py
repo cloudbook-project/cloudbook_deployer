9b8db1e8cad8d82047567377a62bc5cad6a9d436
Garcia Aranda <jose_javier.garcia_aranda@nokia.com>
2019-05-28 13:44:05 +0200

    clean cloudbook

--- cloudbook_deployer.py
+++ cloudbook_deployer.py
@@ -6,12 +6,7 @@ import os
 import sys
 
 """
-	# example of what circle manager must return
-	{
-	"agent_id_0",
-	"agent_id_1"
-	}
-
+	
 #example of du_list.json
 dus = {"du_0":{"cost":0, "size":280},
 "du_1":{"cost":0, "size":260},
@@ -38,104 +33,9 @@ agents = { "agent_id_0": "LOW",
 	"du_0": {"agent_id_0", "agent_id_2", "agent_id_5"}
 	}
 """
-#This function sorts dus in order of costs and size from smaller cost to higher cost.
-def sort_dus_old(dus, configuration = None):
-	dus_tmp={}
-	#Count importance of every du to asign
-	for du in dus:
-		level = dus[du]["cost"] + dus[du]["size"]
-		dus_tmp[du]=level
-	dus_sorted = sorted(dus_tmp.items(), key=lambda kv: kv[1])
-	#print(dus_sorted)	
-	dus_final = dict(dus_sorted)
-	#Format: {'du_6': 50, 'du_4': 92, 'du_5': 133, 'du_0': 200, 'du_1': 251, 'du_3': 315}
-	
-	return dus_final
-
-def sort_dus(dus, configuration = None):
-	dus_tmp={}
-	#Count importance of every du to asign
-	#for du in dus:
-	#	level = dus[du]["cost"] + dus[du]["size"]
-	#	dus_tmp[du]=level
-
-	dus_sorted = sorted(dus,key=operator.itemgetter(1),reverse=True) #, key=lambda kv:kv[1], reverse=True)
-	print "dus ordenadas", dus_sorted
-	return dus_sorted
 
 
 
-#This function sorts agents in order of what they grant from smaller grant to higher grant.
-#High grant means that the agent permits to cloudbook to use much of its computing capacity.
-def sort_agents_old(agents_with_grant, configuration = None):
-	agents_tmp={}
-	#Order the agents by grant
-	for agent in agents_with_grant:
-		if agents_with_grant[agent] == "LOW":
-			agents_tmp[agent]=100
-		elif agents_with_grant[agent] == "MEDIUM":
-			agents_tmp[agent]=200
-		else:
-			agents_tmp[agent]=300
-	agents_sorted = sorted(agents_tmp.items(), key=lambda kv: kv[1])
-	agents_final = dict(agents_sorted)
-	
-	return agents_final
-
-def sort_agents(agents_with_grant, configuration = None):
-	agents_tmp={}
-	#Order the agents by grant
-	for agent in agents_with_grant:
-		if agents_with_grant[agent] == "LOW":
-			agents_tmp[agent]=100
-		elif agents_with_grant[agent] == "MEDIUM":
-			agents_tmp[agent]=200
-		else:
-			agents_tmp[agent]=300
-	print "tmp:", agents_tmp
-	agents_sorted = sorted(agents_tmp, key=lambda kv: kv[1])
-	#agents_final = dict(agents_sorted)
-	#print "agents ordenados", agents_sorted
-	return agents_sorted
-
-#Assigns DUs to agents taking into account the cost each DU and the grant level of every agent.
-def assign_dus_to_machines_old(circle_agents, agents_with_grant, dus, configuration = None):
-
-	dus_sorted=sort_dus(dus)
-	agents_sorted_by_grant=sort_agents(agents_with_grant)
-
-	if(len(circle_agents) != len(agents_sorted_by_grant)):
-		print("FATAL: circle agents number not coincident")
-		return 0
-	else:
-		#Assign agents to dus.
-		#In this case, we're assigning agents to DUs by looking to the most optimal performance.
-		#We assume that LOW means 100 points, MEDIUM 200 points and HIGH 300 points.
-		#Each DU has a number of performance points assigned. When we distribute DUs amongs agents, we substract the performance of 
-		# every agent to the performance of every DU, assigning it when the value is closest to zero. We do this in every case.
-		#With this method we are always assigning every DU to an agent, no matter if there are more DUs that agents.
-		
-		result = {}
-		for du in reversed(list(dus_sorted.keys())):
-			result[du]=[]
-			print(du)
-
-			smallest = int(999999999999)
-			chosen_agent = ""
-			for agent in reversed(list(agents_sorted_by_grant.keys())):
-
-				if abs(dus_sorted[du]-agents_sorted_by_grant[agent]) < smallest:
-					
-					smallest = abs(agents_sorted_by_grant[agent]-dus_sorted[du])
-					print(smallest)
-					chosen_agent = agent
-			
-			result[du].append(chosen_agent)
-			agents_sorted_by_grant[chosen_agent]=agents_sorted_by_grant[chosen_agent]-dus_sorted[du]
-			print(chosen_agent, agents_sorted_by_grant[chosen_agent])
-
-
-		return result
 
 
 def assign_dus_to_machines(circle_agents, agents_with_grant, dus, configuration = None):
