c647370e18217656a993f0e15f5e6517101c807d
jose javier garcia aranda <jjaranda13@hotmail.com>
2020-03-03 10:06:52 +0100

    fix

--- surveillance_monitor.py
+++ surveillance_monitor.py
@@ -96,14 +96,12 @@ def create_file_agents_grant(input_dir):
 	print (agents_files)
 	# enter in each file
 	for file in agents_files:
-<<<<<<< HEAD
 		if file.startswith("agent") and file.endswith(".json"):
 			agent= loader.load_dictionary(input_dir+"/agents_grant/"+file)
 			print ("agent= ",agent)
 			for a in agent:
 				agents_with_grant[a]=agent[a]
 			os.remove (input_dir+"/agents_grant/"+file)
-=======
 		agent= loader.load_dictionary(input_dir+"/agents_grant/"+file)
 		print ("agent= ",agent)
 		for a in agent:
@@ -113,7 +111,6 @@ def create_file_agents_grant(input_dir):
 			os.remove (input_dir+"/agents_grant/"+file)
 		except:
 			pass
->>>>>>> eb97ae38b85bdff89caa92ee4f42cffd2067cc7d
 	print ("all agents have been read. The final output is:")
 	print (agents_with_grant)
 
