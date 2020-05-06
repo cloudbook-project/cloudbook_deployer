87efc94d6a70d86c0cb81780a1a56ea0fd0c79c5
Sergio Molina <sergio.molina_cardin.ext@nokia.com>
2020-05-06 18:05:20 +0200

    Clean cloudbook_run and changed library urllib for requests.

--- cloudbook_run.py
+++ cloudbook_run.py
@@ -1,103 +1,80 @@
+# cloudbook_run.py
+
+# This file retrieves the agent_0 information to call main() function in du_0 to launch the user program.
+
+# Usage:
+#   python3 cloudbook_run.py -project_folder <project_folder>
+
+
+
+#####   IMPORTS   #####
 import loader
-import json, urllib.request
-import urllib.request
-import os, platform
+import os
 import sys
+import platform
 from pathlib import Path
-
-#Checks if al machines are online by connecting to the ip publisher service
-#If so, calls du_0 to run.
-
-#Reads file created by deployer, compares to 
-cloudbook_directory = loader.load_cloudbook("./prueba.json")
-
-def run(circle_id, configuration = None):
-
-    #Process cloudbook_directory by getting every agent that it includes
-    agents_list = []
-    for du in cloudbook_directory:
-        for agent in cloudbook_directory[du]:
-            if agent not in agents_list:
-                agents_list.append(agent)
-
-    ip_publisher_agents = get_online_agents(circle_id)
-
-    if(set(agents_list) == set(ip_publisher_agents)):
-        #Invoke du_0 somehow
-        return True
-    else:
-        print ("Error. The following agents are not online:")
-        print (set(agents_list).symmetric_difference(set(ip_publisher_agents)))
-        return False
+import requests
 
 
-def get_online_agents(circle_id, configuration = None):
-    agents = list()
-    #By now its a fake URL, but it should be:
-    #url = "http://cloudbook-ip-publisher.eu-west-1.elasticbeanstalk.com/getCircle?circle_id="+circle_id"
-    url = "http://localhost:3100/getCircle?circle_id="+circle_id
-    with urllib.request.urlopen(url) as res:
-        data = json.loads(res.read().decode())
-        for agent in data:
-            if data[agent]["IP"]=="None":
-                continue
-            else:
-                agents.append(agent)
-        return agents
 
-
-def run_local(configuration = None):
-    
-    #just invoke du_0 somehow
-    return True
-
-
+#####   MAIN   #####
 if __name__ == "__main__":
 
+	# Process input parameters
 	project_folder = ""
-	num_param=len(sys.argv)
-	for i in range(1,len(sys.argv)):
+	num_param = len(sys.argv)
+	for i in range(1, len(sys.argv)):
 		if sys.argv[i]=="-project_folder":
 			project_folder=	sys.argv[i+1]
-			i=i+1
-	#-----------------------------
-	if (project_folder==""):
+			i = i+1
+
+	if project_folder=="":
 		print ("option -project_folder missing")
 		sys.exit(0)
-	# Variable for cloudbook path
-	if(platform.system()=="Windows"):
-		path = os.environ['HOMEDRIVE'] + os.environ['HOMEPATH']+os.sep+"cloudbook"+os.sep+project_folder
-		print("path", path)
+
+	# Variable for the path to cloudbook
+	if platform.system()=="Windows":
+		cloudbook_path = os.environ['HOMEDRIVE'] + os.environ['HOMEPATH'] + os.sep + "cloudbook"
 	else:
-		path = os.environ['HOME'] + os.sep + "cloudbook" + os.sep + project_folder
-	#Load agent config file
+		cloudbook_path = os.environ['HOME'] + os.sep + "cloudbook"
+
+	# The path to the necessary folders
+	project_path = cloudbook_path + os.sep + project_folder
+	agents_grant_path = project_path + os.sep + "distributed" + os.sep + "agents_grant.json"
+	running_path = project_path + os.sep + "distributed" + os.sep + "RUNNING"
+	cloudbookjson_path = project_path + os.sep + "distributed" + os.sep + "cloudbook.json"
+
+	# Load agent_0 ip and port
 	agent0_ip_and_port = ""
 	try:
-		print(path+os.sep+"distributed"+os.sep+"agents_grant.json")
-		local_ip_info_dict = loader.load_dictionary(path+os.sep+"distributed"+os.sep+"agents_grant.json")
-		agent0_ip_and_port = local_ip_info_dict['agent_0']['IP']+":"+str(local_ip_info_dict['agent_0']['PORT'])
-		print(agent0_ip_and_port)
+		agents_grant_dict = loader.load_dictionary(agents_grant_path)
+		agent0_ip_and_port = agents_grant_dict['agent_0']['IP']+":"+str(agents_grant_dict['agent_0']['PORT'])
+		print("Detected agent_0 is available at:", agent0_ip_and_port)
 	except:
 		print("\nERROR: IP or port for agent_0 is unknown, and execution could not start. See agents_grant.json\n")
 		os._exit(1)
-	
-	#creation of RUNNING file
-	p = Path(path+os.sep+"distributed"+os.sep+"RUNNING")
-	p.touch(exist_ok=True)
 
-	contents = urllib.request.urlopen("http://" + agent0_ip_and_port + "/invoke?invoked_function=du_0.main").read()
-	
-	#deletion of running file
-	running_file= os.path.isfile(path+os.sep+"distributed"+os.sep+"RUNNING")
-	if running_file:
-		os.remove(path+os.sep+"distributed"+os.sep+"RUNNING")
+	# Create RUNNING file
+	Path(running_path).touch(exist_ok=True)
 
-    #contents =
-	#urllib.request.urlopen("http://localhost:3000/invoke?invoked_function=du_0.main").read()
+	# Launch the user program by sending a request to the agent_0, which starts the main() function
+	response = requests.Session().get("http://" + agent0_ip_and_port + "/invoke?invoked_function=du_0.main")
+	print("The program has ended.")
+
+	# Remove RUNNING and cloudbook.json files (user program has ended)
+	if os.path.exists(running_path):
+		os.remove(running_path)
+	if os.path.exists(cloudbookjson_path):
+		os.remove(cloudbookjson_path)
+
+	# Print the result returned by main() function in the user program
 	try:
-		print(eval(contents))
-		os.remove(path+os.sep+"distributed"+os.sep+"cloudbook.json")
+		readed_response = response.json()
 	except:
-		print(contents)
-		os.remove(path+os.sep+"distributed"+os.sep+"cloudbook.json")
-
+		print("The program did not return anything.")
+		os.exit(0)
+	
+	try:
+		print(eval(readed_response))
+	except:
+		print(readed_response)
